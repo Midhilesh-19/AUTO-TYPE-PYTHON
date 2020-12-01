a520fee2a34b7763e5351a1a329ab27a9435d2f2
Midhilesh <72137078+Midhilesh-19@users.noreply.github.com>
2020-12-01 16:03:10 +0530

    Update autotype.py

--- autotype.py
+++ autotype.py
@@ -1,9 +1,12 @@
+# auto type 
+# using pynput
+# first we have to install and import the modules 
 from pynput.keyboard import Key, Controller
 import time
 
 keyboard = Controller()
 time.sleep(2)
-
+#here we use "for" loop to get repeated
 for char in "THIS TIME I GOT THE ANSWER FULL HEADACHE":
     keyboard.press(char)
     keyboard.release(char)
